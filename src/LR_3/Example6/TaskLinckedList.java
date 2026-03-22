0948e5ab5982e65c7900250672e8180bd4e74008
JellyBean-bit <alenaaly29@gmail.com>
2026-03-22 09:37:05 +0500

    add: lab 3

---
+++ src/LR_3/Example6/TaskLinckedList.java
@@ -0,0 +1,26 @@
+package LR_3.Example6;
+
+import java.util.LinkedList;
+
+public class TaskLinckedList {
+    public static void main(String[] args) {
+        int N = 100000;
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            list.add(i);
+        }
+
+        long startTime = System.currentTimeMillis();
+
+        while (list.size() > 1) {
+            list.addLast(list.removeFirst());
+            list.removeFirst();
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Последний оставшийся (LinkedList): " + list.get(0));
+        System.out.println("Время выполнения (LinkedList): " + (endTime - startTime) + " мс");
+    }
+}
